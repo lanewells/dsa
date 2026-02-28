7a14b283a243819203f0fe94d4b9f5d9bbba46a5
lanewells <180503664+lanewells@users.noreply.github.com>
2026-02-28 15:42:08 -0800

    python reference doc

---
+++ reference.py
@@ -0,0 +1,80 @@
+# Python Reference Doc
+
+variable = 'lowercase_snake_case'
+CONSTANT = 'UPPERCASE_SCREAMING_SNAKE_CASE'
+
+# single line comment
+''' multiline 
+comment '''
+
+string = 'string'
+integer = 2
+floating_point = 3.1415926535
+boolean = True
+none_type = None
+
+# str(obj)        # Converts object to a string
+# int(obj, base)  # Converts object to an integer with the provided `base`
+# float(obj)      # Converts object to a floating-point number
+# hex(int)        # Converts integer to a hexadecimal string
+# oct(int)        # Converts integer to an octal string
+# tuple(obj)      # Converts object to a tuple
+# list(obj)       # Converts object to a list
+# dict(obj)       # Converts object to a dictionary
+
+## Performing operators
+
+""" Math
+
+Addition                +
+Subtraction             -
+Multiplication          *
+Division                /
+Modulo                  %
+Exponentiation          **
+Truncated division      //
+
+"""
+
+# shortcut operators
+
+num = 0
+
+num = num + 1
+# can be
+num += 1
+
+num = num / 5
+# can be
+num /= 5
+
+num = num * 3
+# can be
+num *= 3
+
+# and so on with other operators
+
+# Python does not use ++ or -- like JS does
+
+
+
+# STRINGS
+
+my_string = "A double-quoted string"
+your_string = 'A single quoted string'
+multiline_string = '''This is my string that
+                      goes on multiple lines
+                      for whatever reason'''
+multiline_string = """This is another string that
+                      goes on multiple lines
+                      for whatever reason"""
+
+# Concatenation
+
+state = "Hawaii"
+year = 1959
+message = f"{state} joined the U.S. in {year}."
+print(message)  # prints: Hawaii joined the U.S. in 1959.
+
+print("this thang".split(" "))
+print("this.thang".split("."))  # prints: ['this', 'thang']
