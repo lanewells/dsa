2c3d31c7706ab241ab5207e76781bdb192d1d88c
lanewells <180503664+lanewells@users.noreply.github.com>
2026-03-05 13:06:04 -0800

    restructure directory

--- reference.py
+++ reference.py
@@ -22,6 +22,12 @@ none_type = None
 # list(obj)       # Converts object to a list
 # dict(obj)       # Converts object to a dictionary
 
+# Data Types
+
+# 
+
+
+
 ## Performing operators
 
 """ Math
@@ -79,6 +85,56 @@ print(message)  # prints: Hawaii joined the U.S. in 1959.
 print("this thang".split(" "))
 print("this.thang".split("."))  # prints: ['this', 'thang']
 
+
+# String Methods
+
+
+# capitalize()	    Converts the first character to upper case
+# casefold()	    Converts string into lower case
+# center()	        Returns a centered string
+# count()	        Returns the number of times a specified value occurs in a string
+# encode()	        Returns an encoded version of the string
+# endswith()	    Returns true if the string ends with the specified value
+# expandtabs()	    Sets the tab size of the string
+# find()	        Searches the string for a specified value and returns the position of where it was found
+# format()	        Formats specified values in a string
+# format_map()  	Formats specified values in a string
+# index()	        Searches the string for a specified value and returns the position of where it was found
+# isalnum()	        Returns True if all characters in the string are alphanumeric
+# isalpha()	        Returns True if all characters in the string are in the alphabet
+# isascii()	        Returns True if all characters in the string are ascii characters
+# isdecimal()	    Returns True if all characters in the string are decimals
+# isdigit()	        Returns True if all characters in the string are digits
+# isidentifier()	Returns True if the string is an identifier
+# islower()	        Returns True if all characters in the string are lower case
+# isnumeric()	    Returns True if all characters in the string are numeric
+# isprintable()	    Returns True if all characters in the string are printable
+# isspace()	        Returns True if all characters in the string are whitespaces
+# istitle()	        Returns True if the string follows the rules of a title
+# isupper()	        Returns True if all characters in the string are upper case
+# join()	        Joins the elements of an iterable to the end of the string
+# ljust()	        Returns a left justified version of the string
+# lower()	        Converts a string into lower case
+# lstrip()	        Returns a left trim version of the string
+# maketrans()	    Returns a translation table to be used in translations
+# partition()	    Returns a tuple where the string is parted into three parts
+# replace()	        Returns a string where a specified value is replaced with a specified value
+# rfind()	        Searches the string for a specified value and returns the last position of where it was found
+# rindex()	        Searches the string for a specified value and returns the last position of where it was found
+# rjust()	        Returns a right justified version of the string
+# rpartition()	    Returns a tuple where the string is parted into three parts
+# rsplit()	        Splits the string at the specified separator, and returns a list
+# rstrip()	        Returns a right trim version of the string
+# split()	        Splits the string at the specified separator, and returns a list
+# splitlines()	    Splits the string at line breaks and returns a list
+# startswith()	    Returns true if the string starts with the specified value
+# strip()	        Returns a trimmed version of the string
+# swapcase()	    Swaps cases, lower case becomes upper case and vice versa
+# title()	        Converts the first character of each word to upper case
+# translate()	    Returns a translated string
+# upper()	        Converts a string into upper case
+# zfill()	        Fills the string with a specified number of 0 values at the beginning
+
 # Arrays & Iteration Basics
 ## List Comprehension
 
@@ -110,6 +166,21 @@ for x in a:
         print(x)
 
 
+# List Methods
+
+# append()	    Adds an element at the end of the list              ## modifies actual list (would return: None)
+# clear()	    Removes all the elements from the list              ## modifies actual list (would return: None)
+# copy()	    Returns a copy of the list                          ## creates new list
+# count()	    Returns the number of elements with the specified value     ## does nothing to list
+# extend()	    Add the elements of a list (or any iterable), to the end of the current list        ## modifies actual list (would return: None)
+# index()	    Returns the index of the first element with the specified value ## does nothing to list
+# insert()	    Adds an element at the specified position           ## modifies actual list (would return: None)
+# pop()	        Removes the element at the specified position       ## modifies actual list (would return: None)
+# remove()	    Removes the item with the specified value           ## modifies actual list (would return: None)
+# reverse()	    Reverses the order of the list                      ## modifies actual list (would return: None)
+# sort()	    Sorts the list                                      ## modifies actual list (would return: None)
+
+
 # Sets
 
 # set: a collection of unique values (no duplicates) + order doesn't matter
@@ -122,3 +193,26 @@ print(set(a_list))
 # convert back into list
 print(list(set(a_list)))
 # => [1, 2, 3]
+
+
+# Conditionals / Logic
+
+#   ==          equal to                checks if two things are the same
+#   !=          not equal to            checks if two things are different
+#   >           greater than            left value bigger than right
+#   <           less than               left value smaller than right
+#   >=          greater or equal        left >= right
+#   <=          less or equal           left <= right
+
+#   and         both must be true       condition1 AND condition2
+#   or          one must be true        condition1 OR condition2
+#   not         opposite                flips true → false 
+
+#   in          exists in               checks if value is inside list/string/set
+#   not in      does not exist in       checks if value NOT inside
+
+# Sets / list comparisons
+
+#   &           intersection            what both sets have in common
+#   |           union                   everything from both sets combined
+#   -           difference              what's in first set but not second        
