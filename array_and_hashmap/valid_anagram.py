f7bdf35a7676a3e119968092b6a22b7198e3ead5
lanewells <180503664+lanewells@users.noreply.github.com>
2026-03-16 22:13:58 -0700

    valid anagram problem

---
+++ array_and_hashmap/valid_anagram.py
@@ -0,0 +1,15 @@
+# Valid Anagram
+    # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# Example 1:
+    # Input: s = "anagram", t = "nagaram"
+    # Output: true
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
