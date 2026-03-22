f4076e710b840f588bb7c177fe49652b711d723c
lanewells <180503664+lanewells@users.noreply.github.com>
2026-03-21 19:48:21 -0700

    valid anagram solution: using sort

--- array_and_hashmap/valid_anagram.py
+++ array_and_hashmap/valid_anagram.py
@@ -24,3 +24,13 @@ class Solution(object):
             if countS[c] != countT.get(c, 0):
                 return False
         return True
+    
+
+    class Solution(object):
+        def isAnagram(self, s, t):
+            """
+            :type s: str
+            :type t: str
+            :rtype: bool
+            """
+            return sorted(s) == sorted(t)
